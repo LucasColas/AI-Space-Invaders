875c6c235471f8d7b55fd8ca2def9634ce41de39
MrRex <lucas.colas13@gmail.com>
2020-04-16 18:35:40 +0200

    Images loaded

--- SI.py
+++ SI.py
@@ -1,1 +1,16 @@
+import pygame
+import os
+import time
+import random
 
+Red_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+Blue_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+Green_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+Yellow_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
+
+Blue_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+Red_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+Yellow_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+Green_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+
+Bg = pygame.image.load(os.path.join("assets", "background-black.png"))
