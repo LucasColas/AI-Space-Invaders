8091951d25b7e2a454dc868e8d6501b641d6c20d
MrRex <lucas.colas13@gmail.com>
2020-04-17 21:31:48 +0200

    update.

--- SI.py
+++ SI.py
@@ -5,7 +5,7 @@ import random
 
 Width, Height = 1280, 720
 Win = pygame.display.set_mode((Width, Height))
-pygame.display.set_caption("AI Space Invaders")
+pygame.display.set_caption("Space Invaders")
 
 Red_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 Blue_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
@@ -17,24 +17,25 @@ Red_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 Yellow_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 Green_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 
-Bg = pygame.image.load(os.path.join("assets", "background-black.png"))
+Bg = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (Width, Height))
 
 
 def main():
     run = True
-
     FPS = 120
     clock = pygame.time.Clock()
 
-
+    def redraw_window():
+        Win.blit(Bg, (0,0))
+        pygame.display.update()
 
     while run:
         clock.tick(FPS)
+        redraw_window()
 
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 run = False
-
 
 main()
