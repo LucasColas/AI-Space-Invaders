b63253f7da1110ac599dab0027c7a9b1f68baf48
MrRex <lucas.colas13@gmail.com>
2020-04-21 11:56:42 +0200

    update

--- SI.py
+++ SI.py
@@ -6,7 +6,7 @@ pygame.font.init()
 
 WIDTH, HEIGHT = 1280, 720
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Shooter Tutorial")
+pygame.display.set_caption("Space Invaders")
 
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
