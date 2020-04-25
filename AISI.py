cc28fc0a3689b41f670531773bf6cbacc0a70af8
MrRex <lucas.colas13@gmail.com>
2020-04-25 10:50:54 +0200

    update

--- AISI.py
+++ AISI.py
@@ -296,25 +296,6 @@ def main(genomes, config):
             increase_fitness = 10
             for g in ge:
                 g.fitness += increase_fitness
-"""
-def main_menu():
-    title_font = pygame.font.SysFont("comicsans", 70)
-    run = True
-    while run:
-        WIN.blit(BG, (0,0))
-        title_label = title_font.render("Press the mouse to begin...", 1, (255,255,255))
-        WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, 350))
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                main()
-    pygame.quit()
-
-
-main_menu()
-"""
 
 
 def run(config_path):
