5f563380fb033b457ae2ee65979abcc47f3848ee
MrRex <lucas.colas13@gmail.com>
2020-05-11 13:31:15 +0200

    update

--- AISI.py
+++ AISI.py
@@ -180,7 +180,7 @@ def collide(obj1, obj2):
 
 def main(genomes, config):
     run = True
-    FPS = 500
+    FPS = 60
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
