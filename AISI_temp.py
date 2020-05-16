07768f8753f3511ca8756c1913f01f607f51972a
MrRex <lucas.colas13@gmail.com>
2020-05-16 10:12:36 +0200

    update

--- AISI_temp.py
+++ AISI_temp.py
@@ -181,7 +181,7 @@ def collide(obj1, obj2):
 def main(genomes, config):
     run = True
     FPS = 500
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
     lost_font = pygame.font.SysFont("comicsans", 60)
